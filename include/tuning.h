8df128104b7e1f64bad44b04c96895c47dec48db
William Ching <53403691+WillChing01@users.noreply.github.com>
2023-08-19 22:41:50 +0100

    PIN-5 gradient descent (#66)
    feat: pst gradient descent

--- include/tuning.h
+++ include/tuning.h
@@ -2,6 +2,7 @@
 #define TUNING_H_INCLUDED
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -16,155 +17,170 @@
 std::array<int,6> piece_values_start =
 {{
     20000,
-    807,
-    363,
-    303,
-    275,
+    759,
+    339,
+    286,
+    263,
     100,
 }};
 
 std::array<int,6> piece_values_end =
 {{
     20000,
-    1359,
-    730,
-    433,
-    411,
-    135,
+    1694,
+    901,
+    542,
+    517,
+    169,
 }};
 
 std::array<std::array<int,64>,6> piece_tables_start =
 {{
     {{
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -20,-30,-30,-40,-40,-30,-30,-20,
-        -10,-20,-20,-20,-20,-20,-20,-10,
-         20, 20,  0,  0,  0,  0, 20, 20,
-         20, 30, 10,  0,  0, 10, 30, 20,
+  0,  0,  0,  0,  0,  0,  0,-28,
+  0,  0,  0,  0,  0,  0,  0, -2,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0, -6,
+  0,  0,  0,  0,  0,  0,  0,  0,
+ 16, 12,  0,  0,  0,  0, 18, 20,
+  0, 30,  7,  0,  0,  0, 30, 13,
     }},
     {{
-        -20,-10,-10, -5, -5,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5,  5,  5,  5,  0,-10,
-         -5,  0,  5,  5,  5,  5,  0, -5,
-          0,  0,  5,  5,  5,  5,  0, -5,
-        -10,  5,  5,  5,  5,  5,  0,-10,
-        -10,  0,  5,  0,  0,  0,  0,-10,
-        -20,-10,-10, -5, -5,-10,-10,-20,
+ -1,  0,  0,  0,  0,  0,  0,  0,
+ -8,  0,  0,  0,  0,  0,  0,  0,
+ -8,  0,  0,  5,  5,  5,  0,  0,
+ -3,  0,  0,  5,  5,  5,  0,  0,
+  0,  0,  0,  5,  5,  5,  0,  0,
+ -7,  4,  5,  0,  0,  0,  0,  0,
+-10,  0,  0,  0,  0,  0,  0,-10,
+ -8,-10,-10, -3, -5,-10,-10,-20,
     }},
     {{
-          0,  0,  0,  0,  0,  0,  0,  0,
-          5, 10, 10, 10, 10, 10, 10,  5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-          0,  0,  0,  5,  5,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  5, 10, 10, 10, 10, 10, 10,  5,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
+ -5,  0,  0,  0,  0,  0,  0,  0,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+  0,  0,  0,  5,  0,  0,  0,  0,
     }},
     {{
-        -20,-10,-10,-10,-10,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5, 10, 10,  5,  0,-10,
-        -10,  5,  5, 10, 10,  5,  5,-10,
-        -10,  0, 10, 10, 10, 10,  0,-10,
-        -10, 10, 10, 10, 10, 10, 10,-10,
-        -10,  5,  0,  0,  0,  0,  5,-10,
-        -20,-10,-10,-10,-10,-10,-10,-20,
+-18,  0, -8, -8, -8, -8,  0,-11,
+ -8,  0,  0,  0,  0,  0,  0, -5,
+ -8,  0,  5, 10, 10,  5,  0,  0,
+ -8,  5,  5, 10, 10,  5,  5,  0,
+ -8,  0, 10, 10, 10, 10,  0,  0,
+ -4,  3,  6, 10,  7,  0,  0,  0,
+ -3,  0,  0,  0,  0,  0,  0,  0,
+ -4,  0,-10,-10,-10,-10,-10,-20,
     }},
     {{
-        -50,-40,-30,-30,-30,-30,-40,-50,
-        -40,-20,  0,  0,  0,  0,-20,-40,
-        -30,  0, 10, 15, 15, 10,  0,-30,
-        -30,  5, 15, 20, 20, 15,  5,-30,
-        -30,  0, 15, 20, 20, 15,  0,-30,
-        -30,  5, 10, 15, 15, 10,  5,-30,
-        -40,-20,  0,  5,  5,  0,-20,-40,
-        -50,-40,-30,-30,-30,-30,-40,-50,
+-48,-38,-28, -8,  0,-20,  0,-48,
+-38,-18,  0,  0,  0,  0, -4,  0,
+-28,  0, 10, 15, 15, 10,  0,  0,
+-13,  5, 15, 20, 20, 15,  5,  0,
+-16,  0,  8, 10, 15, 15,  0,  0,
+-27,  0,  0,  8,  6,  0,  0,-14,
+-40,-20,  0,  0,  0,  0,-20,-30,
+-50,-28,-30,-30,-30,-30,-36,-50,
     }},
     {{
-       100,100,100,100,100,100,100,100,
-        50, 50, 50, 50, 50, 50, 50, 50,
-        10, 10, 20, 30, 30, 20, 10, 10,
-         5,  5, 10, 25, 25, 10,  5,  5,
-         0,  0,  0, 20, 20,  0,  0,  0,
-         5, -5,-10,  0,  0,-10, -5,  5,
-         5, 10, 10,-20,-20, 10, 10,  5,
-         0,  0,  0,  0,  0,  0,  0,  0,
+100,100,100,100,100,100,100,100,
+ 50, 50, 50, 50, 50, 50, 50, 50,
+ 10, 10, 20, 30, 30, 20, 10, 10,
+  5,  5,  3, 16, 22, 10,  5,  2,
+  0,  0,  0,  0,  1,  0,  0,  0,
+  0, -1, -4,  0,  0, -7,  0,  0,
+  0,  0,  0,-10,-10, 10,  9,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
     }},
 }};
 
 std::array<std::array<int,64>,6> piece_tables_end =
 {{
     {{
-        -50,-40,-30,-20,-20,-30,-40,-50,
-        -30,-20,-10,  0,  0,-10,-20,-30,
-        -30,-10, 20, 30, 30, 20,-10,-30,
-        -30,-10, 30, 40, 40, 30,-10,-30,
-        -30,-10, 30, 40, 40, 30,-10,-30,
-        -30,-10, 20, 30, 30, 20,-10,-30,
-        -30,-30,  0,  0,  0,  0,-30,-30,
-        -50,-30,-30,-30,-30,-30,-30,-50,
+-48, -5,  0,  0, -3, -8,  0,-48,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0, 20, 30, 30, 20,  0,  0,
+  0,  0, 30, 40, 40, 30,  0,  0,
+-19,  0, 30, 37, 37, 30,  0, -9,
+-29, -5,  6, 15, 15, 11,  0,-18,
+-24,-22,  0,  0,  0,  0, -5,-23,
+-50,-21,-20,-30,-30,-30,-18,-50,
     }},
     {{
-        -20,-10,-10, -5, -5,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5,  5,  5,  5,  0,-10,
-         -5,  0,  5,  5,  5,  5,  0, -5,
-          0,  0,  5,  5,  5,  5,  0, -5,
-        -10,  5,  5,  5,  5,  5,  0,-10,
-        -10,  0,  5,  0,  0,  0,  0,-10,
-        -20,-10,-10, -5, -5,-10,-10,-20,
+  0,  0,  0,  0,  0,  0,  0,  0,
+ -8,  0,  0,  0,  0,  0,  0,  0,
+ -8,  0,  5,  5,  5,  5,  0,  0,
+ -3,  0,  0,  5,  5,  5,  0,  0,
+  0,  0,  4,  5,  5,  5,  0,  0,
+-10,  0,  4,  0,  0,  0,  0,  0,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-17,-10,-10, -5, -5,-10,-10,-20,
     }},
     {{
-          0,  0,  0,  0,  0,  0,  0,  0,
-          5, 10, 10, 10, 10, 10, 10,  5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-         -5,  0,  0,  0,  0,  0,  0, -5,
-          0,  0,  0,  5,  5,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  5, 10, 10, 10, 10, 10, 10,  5,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
+ -3,  0,  0,  0,  0,  0,  0, -3,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+  0,  0,  0,  0,  0,  0,  0,  0,
     }},
     {{
-        -20,-10,-10,-10,-10,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5, 10, 10,  5,  0,-10,
-        -10,  5,  5, 10, 10,  5,  5,-10,
-        -10,  0, 10, 10, 10, 10,  0,-10,
-        -10, 10, 10, 10, 10, 10, 10,-10,
-        -10,  5,  0,  0,  0,  0,  5,-10,
-        -20,-10,-10,-10,-10,-10,-10,-20,
+ -1,  0,  0,  0,  0, -9, -4,-10,
+-10,  0,  0,  0,  0,  0,  0, -8,
+ -1,  0,  5, 10, 10,  5,  0,  0,
+ -2,  5,  5, 10, 10,  5,  5,  0,
+-10,  0, 10, 10, 10, 10,  0, -3,
+ -8,  3, 10, 10, 10,  0,  0, -2,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-20,-10,-10,-10,-10,-10,-10,-20,
     }},
     {{
-        -50,-40,-30,-30,-30,-30,-40,-50,
-        -40,-20,  0,  0,  0,  0,-20,-40,
-        -30,  0, 10, 15, 15, 10,  0,-30,
-        -30,  5, 15, 20, 20, 15,  5,-30,
-        -30,  0, 15, 20, 20, 15,  0,-30,
-        -30,  5, 10, 15, 15, 10,  5,-30,
-        -40,-20,  0,  5,  5,  0,-20,-40,
-        -50,-40,-30,-30,-30,-30,-40,-50,
+-48,-16, -7, -3,  0,  0,-20,-48,
+-40,-16,  0,  0,  0,  0,-16,-35,
+-28,  0, 10, 15, 15, 10,  0,-12,
+-20,  2, 15, 20, 20, 15,  5,  0,
+-23,  0, 15, 20, 20, 15,  0, -5,
+-30,  0,  0,  3,  2,  0,  0,-29,
+-40,-20,  0,  0,  0,  0,-20,-40,
+-50,-40,-30,-30,-30,-30,-40,-50,
     }},
     {{
-       100,100,100,100,100,100,100,100,
-        50, 50, 50, 50, 50, 50, 50, 50,
-        10, 10, 20, 30, 30, 20, 10, 10,
-         5,  5, 10, 25, 25, 10,  5,  5,
-         0,  0,  0, 20, 20,  0,  0,  0,
-         5, -5,-10,  0,  0,-10, -5,  5,
-         5, 10, 10,-20,-20, 10, 10,  5,
-         0,  0,  0,  0,  0,  0,  0,  0,
+100,100,100,100,100,100,100,100,
+ 50, 50, 50, 50, 50, 50, 50, 50,
+ 10, 10, 20, 30, 30, 20, 10, 10,
+  5,  5, 10,  0,  0,  3,  5,  5,
+  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0, -6,  0,  0,  0,  0,  0,
+  0,  0,  0, -5,  0, 10, 10,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,
     }},
 }};
 
+struct dataSample
+{
+    std::vector<long double> featuresWhite;
+    std::vector<long double> featuresBlack;
+    long double res;
+    long double materialDiffStart;
+    long double materialDiffEnd;
+    long double phaseStart;
+    long double phaseEnd;
+    long double eval;
+};
+
 int N = 0;
 std::vector<std::pair<std::string,long double> > testPositions;
+std::vector<dataSample> dataset;
+std::array<int, 384> evalStart;
+std::array<int, 384> evalEnd;
 
-const long double K = 0.97;
+const long double K = 0.00475;
 
 std::string formatWeight(int weight)
 {
@@ -196,7 +212,7 @@ void saveWeights()
         {
             for (int k=0;k<8;k++)
             {
-                file << formatWeight(piece_tables_start[i][8*j+k]) << ",";
+                file << formatWeight(evalStart[64 * i + 8 * j + k]) << ",";
             } file << std::endl;
         } file << std::endl;
     }
@@ -209,7 +225,7 @@ void saveWeights()
         {
             for (int k=0;k<8;k++)
             {
-                file << formatWeight(piece_tables_end[i][8*j+k]) << ",";
+                file << formatWeight(evalEnd[64 * i + 8 * j + k]) << ",";
             } file << std::endl;
         } file << std::endl;
     }
@@ -233,6 +249,84 @@ void readPositions()
     }
     N = testPositions.size();
     std::cout << "Finished reading " << N << " positions." << std::endl;
+}
+
+void populateEval()
+{
+    for (int i=0;i<6;i++)
+    {
+        for (int j=0;j<64;j++)
+        {
+            evalStart[64 * i + j] = piece_tables_start[i][j];
+            evalEnd[64 * i + j] = piece_tables_end[i][j];
+        }
+    }
+}
+
+void fenToFeatures(std::string fen, long double res)
+{
+    //append to features vector.
+    dataset.push_back(dataSample());
+    dataset.back().res = res;
+
+    Board b;
+    b.setPositionFen(fen);
+
+    dataset.back().phaseStart = b.shiftedPhase / 256.;
+    dataset.back().phaseEnd = (256. - b.shiftedPhase) / 256.;
+
+    U64 temp; U64 x;
+
+    for (int i=0;i<12;i++)
+    {
+        temp = b.pieces[i];
+        while (temp)
+        {
+            x = popLSB(temp);
+            if (i & 1)
+            {
+                //black.
+                dataset.back().materialDiffStart -= piece_values_start[i >> 1];
+                dataset.back().materialDiffEnd -= piece_values_end[i >> 1];
+                dataset.back().featuresBlack.push_back((i >> 1) * 64 + x);
+            }
+            else
+            {
+                //white.
+                dataset.back().materialDiffStart += piece_values_start[i >> 1];
+                dataset.back().materialDiffEnd += piece_values_end[i >> 1];
+                dataset.back().featuresWhite.push_back((i >> 1) * 64 + (x ^ 56));
+            }
+        }
+    }
+}
+
+void populateDataset()
+{
+    for (int i=0;i<N;i++)
+    {
+        fenToFeatures(testPositions[i].first, testPositions[i].second);
+    }
+}
+
+inline long double evalFeatures(dataSample &data)
+{
+    long double resStart = data.materialDiffStart;
+    long double resEnd = data.materialDiffEnd;
+
+    for (int i=0;i<(int)data.featuresWhite.size();i++)
+    {
+        resStart += evalStart[data.featuresWhite[i]];
+        resEnd += evalEnd[data.featuresWhite[i]];
+    }
+
+    for (int i=0;i<(int)data.featuresBlack.size();i++)
+    {
+        resStart -= evalStart[data.featuresBlack[i]];
+        resEnd -= evalEnd[data.featuresBlack[i]];
+    }
+
+    return resStart * data.phaseStart + resEnd * data.phaseEnd;
 }
 
 inline int customEval(Board &b)
@@ -334,58 +428,12 @@ inline int customEval(Board &b)
     startTotal += piece_tables_start[0][kingPos ^ 56] - piece_tables_start[0][kingPos2];
     endTotal += piece_tables_end[0][kingPos ^ 56] - piece_tables_end[0][kingPos2];
 
-    return (((startTotal * b.shiftedPhase) + (endTotal * (256 - b.shiftedPhase))) / 256) * (1-2*(int)(b.moveHistory.size() & 1));
-}
-
-int qSearchTuning(Board &b, int alpha, int beta)
-{
-    bool inCheck = b.generatePseudoQMoves(b.moveHistory.size() & 1);
-
-    if (b.moveBuffer.size() > 0)
-    {
-        int bestScore = -INT_MAX;
-
-        if (!inCheck)
-        {
-            //do stand-pat check.
-            bestScore=customEval(b);
-            if (bestScore >= beta) {return bestScore;}
-            alpha = max(alpha,bestScore);
-        }
-
-        int score;
-        b.updateOccupied();
-        vector<pair<U32,int> > moveCache = b.orderQMoves(-INT_MAX);
-        for (int i=0;i<(int)(moveCache.size());i++)
-        {
-            b.makeMove(moveCache[i].first);
-            score = -qSearchTuning(b, -beta, -alpha);
-            b.unmakeMove();
-
-            if (score > bestScore)
-            {
-                if (score >= beta) {return score;}
-                bestScore = score;
-                alpha = max(alpha,score);
-            }
-        }
-
-        return bestScore;
-    }
-    else
-    {
-        //no captures left. evaluate normally.
-        bool inCheck2 = b.generatePseudoMoves(b.moveHistory.size() & 1);
-        
-        if (b.moveBuffer.size() > 0) {return customEval(b);}
-        else if (inCheck2) {return -MATE_SCORE;}
-        else {return 0;}
-    }
+    return (((startTotal * b.shiftedPhase) + (endTotal * (256 - b.shiftedPhase))) / 256);
 }
 
 inline long double sigmoid(const long double x)
 {
-    return 1./(1. + powl(10.,-K*x/400.));
+    return 1./(1. + exp(-K*x));
 }
 
 //calculate mean squared error for a subset of data (allows for threading).
@@ -397,8 +445,7 @@ inline long double errorChild(int startInd, int finishInd)
     for (int i=startInd;i<finishInd;i++)
     {
         b.setPositionFen(testPositions[i].first);
-        score = qSearchTuning(b,-MATE_SCORE,MATE_SCORE);
-        if (b.moveHistory.size() & 1) {score *= -1;}
+        score = customEval(b);
         E += powl(testPositions[i].second - sigmoid(score),2.);
     }
     return E / N;
@@ -445,27 +492,85 @@ inline long double error()
 // {
 //     long double oldError = error();
 //     long double currentError;
+//     long double delta = 0.01;
 //     bool direction = 0;
 //     while (true)
 //     {
 //         std::cout << K << " " << oldError << std::endl;
 //         if (!direction)
 //         {
-//             K += 0.01;
+//             K += delta;
 //             currentError = error();
-//             if (currentError > oldError) {direction = 1; K -= 0.01;}
+//             if (currentError > oldError) {direction = 1; K -= delta;}
 //             else {oldError = currentError;}
 //         }
 //         else if (direction)
 //         {
-//             K -= 0.01;
+//             K -= delta;
 //             currentError = error();
-//             if (currentError > oldError) {K += 0.01; break;}
+//             if (currentError > oldError) {K += delta; delta /= 2.; if (delta < 0.0000001) {break;}}
 //             else {oldError = currentError;}
 //         }
 //     }
 //     std::cout << "Final K of " << K << std::endl;
 // }
+
+void optimiseFeatures(int epochs = 100, long double alpha = 0.00001)
+{
+    std::array<long double, 384> gradStart = {};
+    std::array<long double, 384> gradEnd = {};
+    
+    long double err;
+
+    for (int epoch = 1; epoch <= epochs; epoch++)
+    {
+
+        //reset the gradient.
+        for (int i=0;i<384;i++)
+        {
+            gradStart[i] = 0.;
+            gradEnd[i] = 0.;
+        }
+
+        //update the gradient.
+        for (int i=0;i<N;i++)
+        {
+            long double factor = (dataset[i].res - sigmoid(dataset[i].eval)) * sigmoid(dataset[i].eval);
+            for (int j=0;j<(int)dataset[i].featuresWhite.size();j++)
+            {
+                gradStart[dataset[i].featuresWhite[j]] += factor * dataset[i].phaseStart;
+                gradEnd[dataset[i].featuresWhite[j]] += factor * dataset[i].phaseEnd;
+            }
+            for (int j=0;j<(int)dataset[i].featuresBlack.size();j++)
+            {
+                gradStart[dataset[i].featuresBlack[j]] -= factor * dataset[i].phaseStart;
+                gradEnd[dataset[i].featuresBlack[j]] -= factor * dataset[i].phaseEnd;
+            }
+        }
+
+        //increment pst by gradient.
+        for (int i=0;i<384;i++)
+        {
+            evalStart[i] += gradStart[i] * alpha;
+            evalEnd[i] += gradEnd[i] * alpha;
+        }
+
+        //evaluate positions and error.
+        err = 0.;
+        for (int i=0;i<N;i++)
+        {
+            dataset[i].eval = evalFeatures(dataset[i]);
+            err += powl(dataset[i].res - sigmoid(dataset[i].eval), 2.); 
+        }
+        err /= N;
+
+        std::cout << "Epoch: " << epoch << std::endl;
+        std::cout << std::setprecision(10) << "Error: " << err << std::endl;
+
+        //save weights.
+        saveWeights();
+    }
+}
 
 void optimiseMaterial(int delta = 1)
 {
