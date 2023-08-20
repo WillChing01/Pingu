a32466325b60dba11d9cf5ddfb1f6ef3996a9ea9
WillChing01 <williamching2001@gmail.com>
2023-08-20 20:24:24 +0100

    feat: add perft test action

---
+++ testing/perft.py
@@ -0,0 +1,40 @@
+import os
+import engine
+
+def main():
+    e = engine.Engine()
+
+    errors = 0
+    i = 0
+
+    with open(os.getcwd() + "\\testing\\" + "perftSuite.txt", "r") as file:
+        for line in file.readlines():
+            fen, data = line.rstrip().split("; ")
+            depth, nodes = data.split(" ")
+            depth = depth[1:]
+
+            e.stdin("position " + fen)
+            e.stdin("perft " + depth)
+
+            i += 1
+
+            while True:
+                res = e.readline()
+                if "nodes" in res:
+                    res = res.split(" nodes ")
+                    if res[1].isdigit():
+                        if res[1] != nodes:
+                            errors += 1
+                            print("Position", i, "FAIL, expected " + nodes + " nodes, received " + res[1])
+                        else: print("Position", i, "PASS")
+                    else:
+                        errors += 1
+                        print("Incorrect output format, expected integer")
+                    break
+
+    e.quitCommand()
+
+    return errors
+
+if __name__ == "__main__":
+    main()
