c4ec2eb78619c35401d27d0e0fab41a0f44bec25
WillChing01 <williamching2001@gmail.com>
2023-09-09 23:37:07 +0100

    feat: add testing for move validation functions

---
+++ testing/move_validation_check.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import engine
+
+def main():
+    e = engine.Engine()
+
+    errors = 0
+    i = 0
+
+    with open(os.getcwd() + "\\testing\\" + "perftSuite.txt", "r") as file:
+        for line in file.readlines():
+            fen, data = line.rstrip().split("; ")
+            depth, nodes = data.split(" ")
+            depth = depth[1:]
+
+            e.stdin("position " + fen)
+            e.stdin("test " + depth)
+
+            i += 1
+
+            while True:
+                res = e.readline()
+                if "success" in res:
+                    res = res.split(" success ")
+                    if res[1].isdigit():
+                        if int(res[1]) != 1:
+                            errors += 1
+                            print("Position", i, "FAIL")
+                        else: print("Position", i, "PASS")
+                    else:
+                        errors += 1
+                        print("Incorrect output format, expected integer")
+                    break
+
+    e.quitCommand()
+
+    sys.exit(errors != 0)
+
+if __name__ == "__main__":
+    main()
