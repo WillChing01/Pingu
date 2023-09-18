853bce6875689d2423051a99a75dc4c68994aec8
WillChing01 <williamching2001@gmail.com>
2023-09-18 16:22:24 +0100

    feat: allow null move when side to move has minor pieces

--- include/search.h
+++ include/search.h
@@ -169,7 +169,8 @@ int alphaBeta(Board &b, int alpha, int beta, int depth, int ply, bool nullMoveAl
     }
 
     //null move pruning.
-    if (nullMoveAllowed && !inCheck && depth >= nullMoveDepthLimit && b.phase > 0)
+    if (nullMoveAllowed && !inCheck && depth >= nullMoveDepthLimit &&
+        (b.occupied[side] ^ b.pieces[b._nKing+side] ^ b.pieces[b._nPawns+side]))
     {
         b.makeNullMove();
         int nullScore = -alphaBeta(b, -beta, -beta+1, depth-1-nullMoveR, ply+1, false);
