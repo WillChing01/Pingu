c035f33d6ece2dd677a917e07b1ae8a47c7266bc
WillChing01 <williamching2001@gmail.com>
2023-12-03 15:54:18 +0000

    test: add test for fen generation

---
+++ testing/fen_check.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import engine
+
+def main():
+    e = engine.Engine()
+
+    errors = 0
+    i = 0
+
+    with open(os.getcwd() + "\\testing\\" + "perft_positions.txt", "r") as file:
+        for line in file.readlines():
+            fen = line.rstrip().split("; ")[0]
+
+            e.stdin("position fen " + fen)
+            e.stdin("display")
+
+            i += 1
+
+            while True:
+                res = e.readline()
+                if res.rstrip()[-3:] == "0 1":
+                    if res.rstrip() != fen:
+                        errors += 1
+                        print("Position", i, "FAIL, expected " + fen + " fen, received " + res.rstrip())
+                    else:
+                        print("Position", i, "PASS")
+                    break
+
+    e.quitCommand()
+
+    sys.exit(errors != 0)
+
+if __name__ == "__main__":
+    main()
