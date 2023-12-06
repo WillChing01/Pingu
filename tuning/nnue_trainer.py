d1b7e10db209eb460bb43a3e9ff23ecf999f6750
WillChing01 <williamching2001@gmail.com>
2023-12-06 00:37:33 +0000

    feat: multiprocessing data loading for nnue trainer

--- tuning/nnue_trainer.py
+++ tuning/nnue_trainer.py
@@ -1,14 +1,8 @@
 import pickle
+import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
-
-device = (
-    "cuda" if torch.cuda.is_available()
-    else "cpu"
-)
-
-print("Using device", device)
 
 """
 
@@ -16,36 +10,31 @@ Dataset and dataloader
 
 """
 
-BATCH_SIZE = 1024
-
-training_file = "training_data.pickle"
-validation_file = "validation_data.pickle"
-
 class ChessDataset(Dataset):
-    def __init__(self, file):
-        self.data = []
+    def __init__(self, file, input_count):
+        self.input_count = input_count
+        raw_data = []
         with open(file, "rb") as f:
-            self.data = pickle.load(f)
+            raw_data = pickle.load(f)
+        self.indices = np.zeros((len(raw_data), 32), dtype = np.ushort)
+        self.eval = np.zeros((len(raw_data), 1), dtype = np.short)
+
+        for i in range(len(raw_data)):
+            for j in range(len(raw_data[i][0])):
+                self.indices[i][j] = raw_data[i][0][j]
+            for j in range(len(raw_data[i][0]), 32):
+                self.indices[i][j] = self.indices[i][0]
+            self.eval[i][0] = raw_data[i][1]
+
+        del raw_data
 
     def __len__(self):
-        return len(self.data)
+        return len(self.eval)
 
     def __getitem__(self, idx):
-        # convert fen string into numpy tensor.
-        features = torch.zeros(INPUT_COUNT)
-
-        for x in self.data[idx][0]:
-            features[x] = 1
-
-        label = torch.tensor([self.data[idx][1]])
-
-        return features, label
-
-training_data = ChessDataset(training_file)
-validation_data = ChessDataset(validation_file)
-
-training_dataloader = DataLoader(training_data, batch_size = BATCH_SIZE, shuffle = True)
-validation_dataloader = DataLoader(validation_data, batch_size = BATCH_SIZE, shuffle = True)
+        features = np.zeros(self.input_count, dtype = np.ubyte)
+        features[self.indices[idx]] = 1
+        return torch.Tensor(features), torch.Tensor(self.eval[idx])
 
 """
 
@@ -59,25 +48,16 @@ Structure: fully connected layers
 
 """
 
-SAVED_MODEL_FILE = "saved_model.pth"
-
-INPUT_COUNT = 768
-L1_COUNT = 256
-L2_COUNT = 32
-OUTPUT_COUNT = 1
-
-K = 0.00475
-
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, input_count, l1_count, l2_count, output_count):
         super().__init__()
 
         self.net = nn.Sequential(
-            nn.Linear(INPUT_COUNT, L1_COUNT),
+            nn.Linear(input_count, l1_count),
             nn.ReLU(),
-            nn.Linear(L1_COUNT, L2_COUNT),
+            nn.Linear(l1_count, l2_count),
             nn.ReLU(),
-            nn.Linear(L2_COUNT, OUTPUT_COUNT),
+            nn.Linear(l2_count, output_count),
         )
         print("Created network.")
 
@@ -92,29 +72,19 @@ class NeuralNetwork(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-model = NeuralNetwork().to(device)
-
-try:
-    print("Loading saved model...")
-    model.load_state_dict(torch.load(SAVED_MODEL_FILE))
-except:
-    print("Failed to load saved model.")
-
 """
 
 Training loop
 
 """
 
-LEARNING_RATE = 0.001
-EPOCHS = 100
-
 def custom_loss(output, target):
+    K = 0.00475
     output_scaled = torch.sigmoid(K*output)
     target_scaled = torch.sigmoid(K*target)
     return torch.mean((output_scaled - target_scaled)**2)
 
-def training_loop(dataloader, model, loss_fn, optimizer):
+def training_loop(dataloader, model, loss_fn, optimizer, device):
     """
         Optimize model parameters.
     """
@@ -134,7 +104,7 @@ def training_loop(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), (batch + 1) * len(x)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def validation_loop(dataloader, model, loss_fn):
+def validation_loop(dataloader, model, loss_fn, device):
     """
         Run model with validation data.
     """
@@ -150,14 +120,57 @@ def validation_loop(dataloader, model, loss_fn):
 
     validation_loss /= num_batches
     print(f"Validation Loss: {validation_loss:>8f}")
+    return validation_loss
 
-optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+def main():
 
-for epoch in range(EPOCHS):
-    print(f"Epoch {epoch+1}\n-------------------------------")
-    training_loop(training_dataloader, model, custom_loss, optimizer)
-    validation_loop(validation_dataloader, model, custom_loss)
-    print("Saving model...")
-    torch.save(model.state_dict(), 'epoch_'+str(epoch)+'.pth')
+    LEARNING_RATE = 0.001
+    EPOCHS = 10000
 
-print("Done!")
+    BATCH_SIZE = 1024
+    NUM_WORKERS = 6
+
+    SAVED_MODEL_FILE = "saved_model.pth"
+
+    INPUT_COUNT = 768
+    L1_COUNT = 256
+    L2_COUNT = 32
+    OUTPUT_COUNT = 1
+
+    device = (
+        "cuda" if torch.cuda.is_available()
+        else "cpu"
+    )
+
+    print("Using device", device)
+
+    training_file = "training_data.pickle"
+    validation_file = "validation_data.pickle"
+
+    training_data = ChessDataset(training_file, INPUT_COUNT)
+    validation_data = ChessDataset(validation_file, INPUT_COUNT)
+
+    training_dataloader = DataLoader(training_data, batch_size = BATCH_SIZE, shuffle = True, num_workers = NUM_WORKERS)
+    validation_dataloader = DataLoader(validation_data, batch_size = BATCH_SIZE, shuffle = True, num_workers = NUM_WORKERS)
+
+    model = NeuralNetwork(INPUT_COUNT, L1_COUNT, L2_COUNT, OUTPUT_COUNT).to(device)
+
+    try:
+        print("Loading saved model...")
+        model.load_state_dict(torch.load(SAVED_MODEL_FILE))
+    except:
+        print("Failed to load saved model.")
+
+    optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+    for epoch in range(EPOCHS):
+        print(f"Epoch {epoch+1}\n-------------------------------")
+        training_loop(training_dataloader, model, custom_loss, optimizer, device)
+        validation_loss = validation_loop(validation_dataloader, model, custom_loss, device)
+        print("Saving model...")
+        torch.save(model.state_dict(), 'epoch_'+str(epoch)+'_loss_'+str(round(validation_loss, 10)).replace('.',',')+'.pth')
+
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
