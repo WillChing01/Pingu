d6c6463bcca02470a9ef73e4669e9e09485815ac
WillChing01 <williamching2001@gmail.com>
2023-12-08 22:23:52 +0000

    refactor: export weights directly to cpp header

--- tuning/export_model.py
+++ tuning/export_model.py
@@ -15,22 +15,39 @@ def main():
 
     print("Successfully loaded saved model.")
 
-    for layer in model.net:
-        if type(layer) == torch.nn.Linear:
-            weight = layer.weight.detach().numpy()
-            bias = layer.bias.detach().numpy()
-            np.savetxt(
-                "_nnue_weight_"+str(weight.shape).replace(' ', '')+".txt",
-                weight,
-                fmt = "%.18f",
-                delimiter = ','
-            )
-            np.savetxt(
-                "_nnue_bias_"+str(bias.shape).replace(' ', '')+".txt",
-                bias,
-                fmt = "%.18f",
-                delimiter = ','
-            )
+    with open("../include/weights.h", "w") as f:
+        f.write("#ifndef WEIGHTS_H_INCLUDED\n")
+        f.write("#define WEIGHTS_H_INCLUDED\n")
+        f.write("\n")
+        f.write("#include <array>\n")
+        f.write("\n")
+        for layer in model.net:
+            if type(layer) == torch.nn.Linear:
+                weight = layer.weight.detach().numpy()
+                bias = layer.bias.detach().numpy()
+
+                weight_name = '_'.join([str(i) for i in weight.shape])
+                bias_name = '_'.join([str(i) for i in bias.shape])
+
+                f.write("const std::array<std::array<float, "+str(weight.shape[1])+">, "+str(weight.shape[0])+"> weights_"+weight_name+" =\n")
+                f.write("{{\n")
+                for i in range(weight.shape[0]):
+                    f.write("\t{")
+                    for j in range(weight.shape[1]):
+                        f.write("{:.10f}".format(weight[i][j]))
+                        if j < weight.shape[1] - 1: f.write(", ")
+                    f.write("},\n")
+                f.write("}};\n")
+                f.write("\n")
+                f.write("const std::array<float, "+str(bias.shape[0])+"> bias_"+bias_name+" =\n")
+                f.write("{{\n")
+                for i in range(bias.shape[0]):
+                    f.write("\t")
+                    f.write("{:.10f}".format(bias[i]))
+                    f.write(",\n")
+                f.write("}};\n")
+                f.write("\n")
+        f.write("#endif // WEIGHTS_H_INCLUDED\n")
 
     print("Successfully exported weights and biases.")
 
