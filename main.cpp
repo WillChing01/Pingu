2ed069ef8e50c4852aa5f3feb1ecc74a31dd3f8c
WillChing01 <williamching2001@gmail.com>
2023-12-18 21:03:07 +0000

    refactor: remove old tuning code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,3 @@
-#ifdef TUNING
-
-#include <chrono>
-
-#include "magic.h"
-#include "tuning.h"
-
-int main()
-{
-    populateMagicTables();
-    populateRandomNums();
-
-    readWeights();
-    readPositions();
-
-    optimiseFeatures(10000, 0.00001);
-
-    return 0;
-}
-
-#else
-
 #include "uci.h"
 
 int main()
@@ -32,5 +10,3 @@ int main()
 
     return 0;
 }
-
-#endif
