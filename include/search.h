ecacd3f14f35bcb2da513c40d71a62d97e9aa58e
WillChing01 <williamching2001@gmail.com>
2024-01-01 15:13:10 +0000

    feat: allow lmr to be adjusted

--- include/search.h
+++ include/search.h
@@ -13,6 +13,8 @@
 #include "format.h"
 #include "board.h"
 
+int lmrRatio = 50;
+
 const int maximumPruningDepth = 8;
 
 const int inverseFutilityMargin = 120;
@@ -432,7 +434,7 @@ int alphaBeta(Board &b, int alpha, int beta, int depth, int ply, bool nullMoveAl
         if (depth >= 2 && numMoves > 0)
         {
             //late move reductions (non pv nodes).
-            int LMR = int(0.5 * std::log((double)depth) * std::log((double)(numMoves+1)));
+            int LMR = int(lmrRatio * 0.01 * std::log((double)depth) * std::log((double)(numMoves+1)));
             if (LMR && (alpha == (beta - 1)) && !inCheck)
             {
                 score = -alphaBeta(b, -beta, -alpha, depth-1-LMR, ply+1, true);
