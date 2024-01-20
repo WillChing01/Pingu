9b5870abeb0232c18959bae19ce9921fe3c52aef
WillChing01 <williamching2001@gmail.com>
2024-01-20 19:42:39 +0000

    refactor: define node classes for regular and q search

--- include/node.h
+++ include/node.h
@@ -6,26 +6,57 @@
 
 #include "constants.h"
 
-class Node
+class BaseNode
 {
-    public:
-        bool inCheck;
-        int staticEval;
+    protected:
         std::vector<std::pair<U32,int> > moveCache;
+        int mvv;
+        int lva;
+        U64 victimBB;
+        U64 attackerBB;
+    public:
+        bool side;
+
+        bool inCheck;
+        int numChecks;
+
+        int staticEval;
+
+        int depth;
+
+        int bestScore;
+        int score;
+        U32 bestMove;
+        U32 move;
+};
+
+class Node : public BaseNode
+{
+    protected:
         std::vector<U32> quietsPlayed;
         int numMoves = 0;
 
+    public:
         U64 zHash;
+        bool hashHit;
 
-    Node()
-    {
-        //initiate the node.
-    }
+        Node()
+        {
+            //initiate the node.
+        }
 
-    void reset()
-    {
-        //reset the node for a new position.
-    }
+        void reset()
+        {
+            //reset the node for a new position.
+        }
+};
+
+class QNode : public BaseNode
+{
+    public:
+        QNode() {}
+
+        void reset() {}
 };
 
 #endif // NODE_H_INCLUDED
