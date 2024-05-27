39374cb0c0a17edcd719a9179844b9ee7873b923
WillChing01 <williamching2001@gmail.com>
2024-05-27 23:48:19 +0100

    add game result to gensfen

---
+++ include/gensfen.h
@@ -0,0 +1,13 @@
+#ifndef GENSFEN_H_INCLUDED
+#define GENSFEN_H_INCLUDED
+
+#include <string>
+
+struct gensfenData
+{
+    std::string fen;
+    int eval;
+    double result;
+};
+
+#endif // GENSFEN_H_INCLUDED
