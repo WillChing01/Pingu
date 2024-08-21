809c18252430d0581d9192a8a4a02d35512b20ef
WillChing01 <williamching2001@gmail.com>
2024-08-21 20:48:14 +0100

    remove brackets

--- main.cpp
+++ main.cpp
@@ -11,10 +11,8 @@ int main(int argc, const char* argv[])
     switch(argc)
     {
         case 1:
-        {
             uciLoop();
             break;
-        }
         case 2:
             if (std::strcmp(argv[1], "bench") == 0) {Board b; benchCommand(b);}
             break;
