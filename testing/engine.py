28913f0adf41f898b8d6a2528205ccff6fae1d7a
WillChing01 <williamching2001@gmail.com>
2024-09-22 23:36:56 +0100

    remove redundant commands in python engine interface

--- testing/engine.py
+++ testing/engine.py
@@ -13,10 +13,6 @@ class Engine:
 
     def __init__(self, name = ENGINE_EXE, path = REL_PATH):
         self._process = Popen(os.getcwd() + path + name, stdin = PIPE, stdout = PIPE)
-        self.uciCommand()
-        self.stdin("setoption name Hash value 32")
-        self.stdin("setoption name Threads value 1")
-        self.isReadyCommand()
 
     def stdin(self, string):
         self._process.stdin.write((string + "\n").encode())
