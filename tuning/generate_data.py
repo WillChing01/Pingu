f7356770e69b2f45985a05d4a0e8bba19413d3a1
WillChing01 <williamching2001@gmail.com>
2024-09-29 16:52:42 +0100

    refactor progress bars

--- tuning/generate_data.py
+++ tuning/generate_data.py
@@ -22,33 +22,38 @@ REPO_ID = "WillChing01/pingu"
 PATH_IN_REPO = "Pingu_4.0.0"
 REPO_TYPE = "dataset"
 
-def gensfen_worker(position: int, token: str) -> None:
+def progress_bar(total_positions: int, q: multiprocessing.Queue) -> None:
+    n = 0
+    with tqdm(total=total_positions, desc=f"Overall progress") as progress:
+        for update in iter(q.get, None):
+            progress.update(min(update, total_positions - n))
+            n = min(n + update, total_positions)
+
+def gensfen_worker(token: str, q: multiprocessing.Queue) -> None:
     import engine
     cmd = f"Pingu.exe gensfen mindepth {MINDEPTH} maxdepth {MAXDEPTH} positions {POSITIONS} randomply {RANDOMPLY} maxply {MAXPLY} evalbound {EVALBOUND} hash {HASH} book {BOOK}"
     e = engine.Engine(name=cmd, path="\\..\\")
     previous_n = 0
-    with tqdm(total=POSITIONS, position=position, desc=f"Worker {position}", leave=False) as progress:
-        while True:
-            res = e.readline()
-            if "Finished generating positions" in res:
-                fileName = res.split(" - ")[1]
-                try:
-                    huggingface_hub.upload_file(
-                        path_or_fileobj=fileName,
-                        path_in_repo=f"/{PATH_IN_REPO}/{fileName}",
-                        repo_id=REPO_ID,
-                        repo_type=REPO_TYPE,
-                        token=token
-                    )
-                    os.remove(fileName)
-                except:
-                    print(f"Error uploading file - {fileName}")
-                progress.update(POSITIONS - previous_n)
-                break
-            elif "Finished game" in res:
-                n = min(int(res.split("; ")[1].split(" ")[0]), POSITIONS)
-                progress.update(n - previous_n)
-                previous_n = n
+    while True:
+        res = e.readline()
+        if "Finished generating positions" in res:
+            fileName = res.split(" - ")[1]
+            try:
+                huggingface_hub.upload_file(
+                    path_or_fileobj=fileName,
+                    path_in_repo=f"/{PATH_IN_REPO}/{fileName}",
+                    repo_id=REPO_ID,
+                    repo_type=REPO_TYPE,
+                    token=token
+                )
+                os.remove(fileName)
+            except:
+                print(f"Error uploading file - {fileName}")
+            break
+        elif "Finished game" in res:
+            n = min(int(res.split("; ")[1].split(" ")[0]), POSITIONS)
+            q.put(n - previous_n)
+            previous_n = n
 
 def main():
     user_args = sys.argv[1:]
@@ -71,22 +76,29 @@ def main():
         print("Invalid token.")
         return
 
+    q = multiprocessing.Manager().Queue()
     pool = multiprocessing.Pool(num_cpu)
     result = [None for i in range(num_cpu)]
     total = 0
 
-    with tqdm(total=total_positions, position=num_cpu, desc=f"Overall progress") as progress:
-        while True:
-            for i in range(num_cpu):
-                if result[i] is None or result[i].ready():
-                    if result[i] is not None:
-                        total += POSITIONS
-                        progress.update(max(min(POSITIONS, total_positions - total), 0))
-                        if total >= total_positions:
-                            return
-                    result[i] = pool.apply_async(gensfen_worker, args=(i,token))
-                    time.sleep(5)
-            time.sleep(0.5)
+    progress = multiprocessing.Process(target=progress_bar, args=(total_positions, q))
+    progress.start()
+
+    finished = False
+    while not finished:
+        for i in range(num_cpu):
+            if result[i] is None or result[i].ready():
+                if result[i] is not None:
+                    total += POSITIONS
+                    if total >= total_positions:
+                        finished = True
+                        break
+                result[i] = pool.apply_async(gensfen_worker, args=(token, q))
+                time.sleep(5)
+        time.sleep(0.5)
+
+    q.put(None)
+    progress.join()
 
 if __name__ == "__main__":
     main()
