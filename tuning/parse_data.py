ce2b409ec7f0f4a2dd215d3adc6be4196432f63c
WillChing01 <williamching2001@gmail.com>
2024-10-20 19:38:16 +0100

    update parse data script

--- tuning/parse_data.py
+++ tuning/parse_data.py
@@ -6,63 +6,72 @@ import sys
 import numpy as np
 import multiprocessing
 import huggingface_hub
+from parallelbar import progress_starmap
 from utils import REPO_ID, REPO_TYPE, DATASET_DTYPE
+
 
 def download_files(token: str, directory: str) -> None:
     huggingface_hub.snapshot_download(
         repo_id=REPO_ID,
         repo_type=REPO_TYPE,
-        cache_dir=directory,
-        local_dir=directory,
-        token=token
+        cache_dir=f"\\\\?\\{directory}",
+        local_dir=f"\\\\?\\{directory}",
+        token=token,
     )
 
-def fenToSparse(fen):
-    """Return non-zero indices of input matrix derived from FEN."""
 
-    fen = fen.split(' ')[0]
-    indices = []
+def parse_file(start_index, file_name, dataset_file, dataset_shape):
+    piece_types = {y: x for x, y in enumerate("KkQqRrBbNnPp")}
+    side_mapping = {y: x for x, y in enumerate("wb")}
 
-    square = 56
-    pieceTypes = "KkQqRrBbNnPp"
-
-    for i in range(len(fen)):
-        if fen[i] == '/':
-            square -= 16
-        elif fen[i].isdigit():
-            square += int(fen[i])
-        else:
-            indices.append(64*pieceTypes.find(fen[i]) + square)
-            square += 1
-
-    return indices
-
-def sparseToArray(indices):
-    x = np.zeros(32, dtype = DATASET_DTYPE)
-    x[0:len(indices)] = np.array(indices, copy=True)
-    x[len(indices):].fill(indices[0])
-    return x
-
-def parseFile(startIndex, file_name, dataset_file, dataset_shape):
-    dataset = np.memmap(dataset_file, mode = "r+", dtype = DATASET_DTYPE, shape = dataset_shape)
-
-    i = startIndex
+    index = 0
     with open(file_name, "r") as f:
         lines = f.readlines()
+        length = len(lines)
+
+        data_chunk = np.full((length, 35), -1, dtype=DATASET_DTYPE)
+
         for line in lines:
-            fen, evaluation, result = line.rstrip().split("; ")
+            fen, eval_, result = line.rstrip().split("; ")
+            fen, side_to_move = fen.split(" ")[0:2]
 
-            fenData = sparseToArray(fenToSparse(fen))
-            evalData = np.array([int(evaluation)], dtype = DATASET_DTYPE)
-            resultData = np.array([int(round(2. * float(result)))], dtype = DATASET_DTYPE)
+            side_to_move = side_mapping[side_to_move]
+            eval_ = int(eval_)
+            result = round(2 * float(result))
 
-            dataset[i] = np.array(np.concatenate([fenData, evalData, resultData]), copy = True)
-            i += 1
+            if side_to_move == 1:
+                eval_ = -eval_
+                result = 2 - result
+
+            data_chunk[index][[-3, -2, -1]] = side_to_move, eval_, result
+
+            i = 0
+            square = 56
+            for x in fen:
+                if x == "/":
+                    square -= 16
+                elif x.isdigit():
+                    square += int(x)
+                else:
+                    data_chunk[index][i] = 64 * piece_types[x] + square
+                    if x == "K":
+                        data_chunk[index][[0, i]] = data_chunk[index][[i, 0]]
+                    elif x == "k":
+                        data_chunk[index][[1, i]] = data_chunk[index][[i, 1]]
+                    i += 1
+                    square += 1
+
+            index += 1
+
+    dataset = np.memmap(
+        dataset_file, mode="r+", dtype=DATASET_DTYPE, shape=dataset_shape
+    )
+
+    dataset[start_index : start_index + length] = data_chunk
 
     dataset.flush()
     dataset._mmap.close()
 
-    return True
 
 def main():
     user_args = sys.argv[1:]
@@ -82,7 +91,7 @@ def main():
         print("Invalid token.")
         return
 
-    directory = os.getcwd() + "/datasets/"
+    directory = f"{os.getcwd()}\\datasets\\"
 
     if not os.path.isdir(directory):
         os.mkdir(directory)
@@ -90,58 +99,40 @@ def main():
     if not os.listdir(directory):
         download_files(token, directory)
 
-    fileData = []
+    file_data = []
     n = 0
 
-    print("Searching for data...")
-
-    for (root, dirs, files) in os.walk(directory):
+    for root, _, files in os.walk(directory):
         if len(files) == 0:
             continue
         for file in files:
             if file.split(".")[-1] != "txt":
                 continue
-            fileData.append([n, f"{root}/{file}"])
+            file_data.append((n, f"{root}/{file}"))
             n += int(re.findall(r"_n([1-9][0-9]*)_", file)[0])
 
-    print("Found", n, "pieces of data in", len(fileData), "files.")
+    print("Found", n, "pieces of data in", len(file_data), "files.")
 
-    print("Storing data...")
-
-    dataset_file = "dataset_" + str(n) + "_34.dat"
-    dataset_shape = (n, 34)
+    dataset_file = "dataset_" + str(n) + "_35.dat"
+    dataset_shape = (n, 35)
 
     try:
-        dataset = np.memmap(dataset_file, mode = "r", dtype = DATASET_DTYPE, shape = dataset_shape)
+        dataset = np.memmap(
+            dataset_file, mode="r", dtype=DATASET_DTYPE, shape=dataset_shape
+        )
         dataset._mmap.close()
     except:
-        dataset = np.memmap(dataset_file, mode = "w+", dtype = DATASET_DTYPE, shape = dataset_shape)
+        dataset = np.memmap(
+            dataset_file, mode="w+", dtype=DATASET_DTYPE, shape=dataset_shape
+        )
         dataset._mmap.close()
 
-    pool = multiprocessing.Pool(num_cpu)
-    result = [None for i in range(num_cpu)]
+    args = [(x, y, dataset_file, dataset_shape) for x, y in file_data]
 
-    file_number = 1
-    total_files = len(fileData)
-    fileData.reverse()
-    while True:
-        all_ready = True
-        for i in range(num_cpu):
-            if result[i] is None or result[i].ready():
-                try:
-                    data = fileData.pop()
-                    args = (data[0], data[1], dataset_file, dataset_shape)
-                    print("Reading file", file_number, "/", total_files, ";", data[1])
-                    result[i] = pool.apply_async(parseFile, args = args)
-                    file_number += 1
-                except:
-                    pass
-            else:
-                all_ready = False
-        if all_ready and len(fileData) == 0:
-            break
+    progress_starmap(parse_file, args, n_cpu=num_cpu)
 
-    print("Process complete.")
+    os.rmdir(directory)
+
 
 if __name__ == "__main__":
     main()
