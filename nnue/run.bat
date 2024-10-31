99678d6ce36dedd7b6c8132e3bd65f06d3ff39a4
WillChing01 <williamching2001@gmail.com>
2024-10-31 22:34:11 +0000

    update batch file

--- nnue/run.bat
+++ nnue/run.bat
@@ -1,10 +1,7 @@
-del /Q /S %cd%\_raw
-
-python download.py -d _raw
-
-del /Q /S %cd%\dataset\training
-del /Q /S %cd%\dataset\validation
-
-make clean
-make
+del /Q /S %cd%\_raw && ^
+python download.py -d _raw && ^
+del /Q /S %cd%\dataset\training && ^
+del /Q /S %cd%\dataset\validation && ^
+make clean && ^
+make && ^
 parse.exe -N 6
