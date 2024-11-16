1a44a9194d6e2f199729acabe2e451667db7ab04
WillChing01 <williamching2001@gmail.com>
2024-11-16 00:40:26 +0000

    add quantization config

--- nnue/model/config.py
+++ nnue/model/config.py
@@ -11,3 +11,30 @@ MODEL_PATH = f"{os.getcwd()}\\checkpoints"
 INPUT_COUNT = 45056
 L1_COUNT = 64
 OUTPUT_COUNT = 1
+
+QUANT_CONFIG = {
+    (64, 45056): {
+        "type": "short",
+        "w": {
+            "factor": 127,
+            "clamp": 32767,
+        },
+        "b": {
+            "factor": 127,
+            "clamp": 32767,
+        },
+        "transpose": True,
+    },
+    (1, 128): {
+        "type": "char",
+        "w": {
+            "factor": 64,
+            "clamp": 127,
+        },
+        "b": {
+            "factor": 64 * 127,
+            "clamp": 32767,
+        },
+        "transpose": False,
+    },
+}
