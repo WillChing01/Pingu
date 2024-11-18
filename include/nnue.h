ee7a07369d3714a755f253831d8942d1178fba5d
WillChing01 <williamching2001@gmail.com>
2024-11-18 23:41:17 +0000

    first implementation of accumulator

--- include/nnue.h
+++ include/nnue.h
@@ -11,13 +11,13 @@
 #include "bitboard.h"
 #include "weights.h"
 
-//activation is implicitly ReLU, except for L2 -> Out which is linear.
+// activation is implicitly ReLU, except for L2 -> Out which is linear.
 
 const __m256i _ZERO = _mm256_setzero_si256();
 const __m256i _ONE = _mm256_set1_epi16(1);
 
-//horizontal sum of vector
-//https://stackoverflow.com/questions/60108658/fastest-method-to-calculate-sum-of-all-packed-32-bit-integers-using-avx512-or-av
+// horizontal sum of vector
+// https://stackoverflow.com/questions/60108658/fastest-method-to-calculate-sum-of-all-packed-32-bit-integers-using-avx512-or-av
 
 inline int hsum_epi32_avx(__m128i x)
 {
@@ -32,8 +32,7 @@ inline int hsum_8x32(__m256i v)
 {
     __m128i sum128 = _mm_add_epi32(
         _mm256_castsi256_si128(v),
-        _mm256_extracti128_si256(v, 1)
-    );
+        _mm256_extracti128_si256(v, 1));
     return hsum_epi32_avx(sum128);
 }
 
@@ -53,250 +52,165 @@ inline __m256i madd_epi8(__m256i a, __m256i b)
 
 class Accumulator
 {
-    public:
-        __m256i l1[4] = {};
-        __m256i cl1[2] = {};
+public:
+    const U64 *pieces;
+    bool side;
+    int kingPos;
+    __m256i l1[4] = {};
+    __m256i cl1[2] = {};
 
-        Accumulator() {}
+    Accumulator() {}
 
-        void setOne(int index)
+    Accumulator(const U64 *_pieces, bool _side) : pieces(_pieces), side(_side) {}
+
+    virtual U32 index(U32 pieceType, U32 square) = 0;
+
+    void makeMove(U32 move)
+    {
+        _move(move, &Accumulator::setZero, &Accumulator::setOne);
+    }
+
+    void unmakeMove(U32 move)
+    {
+        _move(move, &Accumulator::setOne, &Accumulator::setZero);
+    }
+
+    void _move(U32 move, void (Accumulator::*_zero)(U32 index), void (Accumulator::*_one)(U32 index))
+    {
+        U32 pieceType = (move & MOVEINFO_PIECETYPE_MASK) >> MOVEINFO_PIECETYPE_OFFSET;
+        if (pieceType == side)
         {
-            for (size_t i=0;i<4;++i)
+            refresh();
+            return;
+        }
+
+        U32 startSquare = (move & MOVEINFO_STARTSQUARE_MASK) >> MOVEINFO_STARTSQUARE_OFFSET;
+        U32 finishPieceType = (move & MOVEINFO_FINISHPIECETYPE_MASK) >> MOVEINFO_FINISHPIECETYPE_OFFSET;
+        U32 finishSquare = (move & MOVEINFO_FINISHSQUARE_MASK) >> MOVEINFO_FINISHSQUARE_OFFSET;
+        U32 capturedPieceType = (move & MOVEINFO_CAPTUREDPIECETYPE_MASK) >> MOVEINFO_CAPTUREDPIECETYPE_OFFSET;
+        bool enPassant = move & MOVEINFO_ENPASSANT_MASK;
+
+        (this->*_zero)(index(pieceType, startSquare));
+        (this->*_one)(index(finishPieceType, finishSquare));
+        if (capturedPieceType != 15)
+        {
+            (this->*_zero)(index(capturedPieceType, finishSquare + enPassant * (-8 + 16 * (pieceType & 1))));
+        }
+
+        // check for castles.
+        if (pieceType == (!side))
+        {
+            if (finishSquare - startSquare == 2)
             {
-                __m256i x = _mm256_loadu_si256((__m256i *)&w_0[index][i * 16]);
-                l1[i] = _mm256_add_epi16(l1[i], x);
+                (this->*_zero)(index(_nRooks + !side, KING_ROOK_SQUARE[!side]));
+                (this->*_one)(index(_nRooks + !side, KING_ROOK_SQUARE[!side] - 2));
+            }
+            else if (startSquare - finishSquare == 2)
+            {
+                (this->*_zero)(index(_nRooks + !side, QUEEN_ROOK_SQUARE[!side]));
+                (this->*_one)(index(_nRooks + !side, QUEEN_ROOK_SQUARE[!side] + 3));
             }
         }
+    }
 
-        void setZero(int index)
+    void refresh()
+    {
+        kingPos = __builtin_ctzll(pieces[side]);
+        std::copy(b_0.begin(), b_0.end(), l1);
+
+        setOne(index(!side, __builtin_ctzll(pieces[!side])));
+        for (size_t i = 2; i < 12; ++i)
         {
-            for (size_t i=0;i<4;++i)
+            U64 x = pieces[i];
+            while (x)
             {
-                __m256i x = _mm256_loadu_si256((__m256i *)&w_0[index][i * 16]);
-                l1[i] = _mm256_sub_epi16(l1[i], x);
+                setOne(index(i, popLSB(x)));
             }
         }
+        cReLU();
+    }
 
-        void cReLU()
+    void setOne(U32 index)
+    {
+        for (size_t i = 0; i < 4; ++i)
         {
-            cl1[0] = _mm256_max_epi16(_ZERO, cvtepi16_epi8(l1[0], l1[1]));
-            cl1[1] = _mm256_max_epi16(_ZERO, cvtepi16_epi8(l1[2], l1[3]));
+            l1[i] = _mm256_add_epi16(l1[i], w_0[index][i]);
         }
+        cReLU();
+    }
+
+    void setZero(U32 index)
+    {
+        for (size_t i = 0; i < 4; ++i)
+        {
+            l1[i] = _mm256_sub_epi16(l1[i], w_0[index][i]);
+        }
+        cReLU();
+    }
+
+    void cReLU()
+    {
+        cl1[0] = _mm256_max_epi16(_ZERO, cvtepi16_epi8(l1[0], l1[1]));
+        cl1[1] = _mm256_max_epi16(_ZERO, cvtepi16_epi8(l1[2], l1[3]));
+    }
+};
+
+class White : public Accumulator
+{
+public:
+    White() : Accumulator() {}
+
+    White(const U64 *_pieces) : Accumulator(_pieces, 0) {}
+
+    U32 index(U32 pieceType, U32 square)
+    {
+        return 704 * kingPos + 64 * (pieceType - 1) + square;
+    }
+};
+
+class Black : public Accumulator
+{
+public:
+    Black() : Accumulator() {}
+
+    Black(const U64 *_pieces) : Accumulator(_pieces, 1) {}
+
+    U32 index(U32 pieceType, U32 square)
+    {
+        return 704 * kingPos + 64 * (pieceType - 2 * (pieceType & 1)) + (square ^ 56);
+    }
 };
 
 class NNUE
 {
-    private:
-        const U64* pieces;
-        const bool* side;
+private:
+    White white;
+    Black black;
+    const bool *side;
 
-        int kingPos[2] = {};
+public:
+    NNUE() {}
 
-        Accumulator white;
-        Accumulator black;
+    NNUE(const U64 *_pieces, const bool *_side) : side(_side)
+    {
+        white = White(_pieces);
+        black = Black(_pieces);
+    }
 
-    public:
-        NNUE() {}
+    void fullRefresh()
+    {
+        white.refresh();
+        black.refresh();
+    }
 
-        NNUE(const U64* _pieces, const bool* _side): pieces(_pieces), side(_side) {}
-
-        void makeMove(U32 move)
-        {
-            U32 pieceType = (move & MOVEINFO_PIECETYPE_MASK) >> MOVEINFO_PIECETYPE_OFFSET;
-            if (pieceType == 0)
-            {
-                refreshWhite();
-            }
-            else if (pieceType == 1)
-            {
-                refreshBlack();
-            }
-            else
-            {
-                //TODO.
-            }
-        }
-
-        void fullRefresh()
-        {
-            refreshWhite();
-            refreshBlack();
-        }
-
-        void refreshWhite()
-        {
-            kingPos[0] = __builtin_ctzll(pieces[0]);
-            std::copy(b_0, b_0 + 64, white.l1);
-
-            white.setOne(704 * kingPos[0] + kingPos[1]);
-            for (size_t i=2;i<12;++i)
-            {
-                U64 x = pieces[i];
-                while (x) {setOne(704 * kingPos[0] + 64 * (i - 1) + popLSB(x));}
-            }
-            white.cReLU();
-        }
-
-        void refreshBlack(U32 square)
-        {
-            kingPos[1] = __builtin_ctzll(pieces[1]);
-            std::copy(b_0, b_0 + 64, black.l1);
-
-            black.setOne(704 * kingPos[1] + (kingPos[0] ^ 56));
-            for (size_t i=2;i<12;++i)
-            {
-                U64 x = pieces[i];
-                while (x) {setOne(704 * kingPos[1] + 64 * (i - 2 * (i & 1)) + (popLSB(x) ^ 56));}
-            }
-            black.cReLU();
-        }
-
-        int forward()
-        {
-            __m256i sum = _ZERO;
-            for (size_t i=0;i<2;++i)
-            {
-                __m256i w = _mm256_loadu_si256((__m256i *)&w_1[0][64 * (*side) + 32 * i]);
-                sum = _mm256_add_epi32(sum, madd_epi8(white.cl1[i], w));
-            }
-            for (size_t i=0;i<2;++i)
-            {
-                __m256i w = _mm256_loadu_si256((__m256i *)&w_1[0][64 * (!(*side)) + 32 * i]);
-                sum = _mm256_add_epi32(sum, madd_epi8(black.cl1[i], w));
-            }
-            return hsum_8x32(sum) + b_1[0];
-        }
+    int forward()
+    {
+        __m256i sum = madd_epi8(white.cl1[0], w_1[0][2 * (*side)]);
+        sum = _mm256_add_epi32(sum, madd_epi8(white.cl1[1], w_1[0][2 * (*side) + 1]));
+        sum = _mm256_add_epi32(sum, madd_epi8(black.cl1[0], w_1[0][2 * (!(*side))]));
+        sum = _mm256_add_epi32(sum, madd_epi8(black.cl1[1], w_1[0][2 * (!(*side)) + 1]));
+        return hsum_8x32(sum) + b_1[0];
+    }
 };
-
-
-
-// class NNUE
-// {
-//     private:
-//         const U64* pieces;
-
-//         short input_layer[INPUT_COUNT] = {};
-//         short input_weights[INPUT_COUNT][L1_COUNT] = {};
-//         short input_bias[L1_COUNT] = {};
-
-//         short l1_layer[L1_COUNT] = {};
-//         short l1_crelu[L1_COUNT] = {};
-//         short l1_weights[L2_COUNT][L1_COUNT] = {};
-//         short l1_bias[L2_COUNT] = {};
-
-//         int l2_layer[L2_COUNT] = {};
-//         int l2_weights[OUTPUT_COUNT][L2_COUNT] = {};
-//         int l2_bias[OUTPUT_COUNT] = {};
-
-//         int output_layer[OUTPUT_COUNT] = {};
-
-//     public:
-//         NNUE() {}
-
-//         NNUE(const U64* _pieces)
-//         {
-//             pieces = _pieces;
-//             readWeights();
-//         }
-
-//         void refreshInput()
-//         {
-//             //refresh the input layer.
-//             for (int i=0;i<INPUT_COUNT;i++) {input_layer[i] = 0;}
-
-//             for (int i=0;i<12;i++)
-//             {
-//                 U64 x = pieces[i];
-//                 while (x) {input_layer[64 * i + popLSB(x)] = 1;}
-//             }
-
-//             //update l1 layer.
-//             for (int i=0;i<L1_COUNT;i++)
-//             {
-//                 l1_layer[i] = input_bias[i];
-//                 for (int j=0;j<INPUT_COUNT;j++)
-//                 {
-//                     l1_layer[i] += input_weights[j][i] * input_layer[j];
-//                 }
-//             }
-//         }
-
-//         void zeroInput(int idx)
-//         {
-//             //update first hidden layer, assuming input bit set to zero.
-//             __m256i x, y;
-//             for (int i=0;i<L1_COUNT;i+=16)
-//             {
-//                 x = _mm256_loadu_si256((__m256i *)&input_weights[idx][i]);
-//                 y = _mm256_loadu_si256((__m256i *)&l1_layer[i]);
-//                 y = _mm256_sub_epi16(y, x);
-//                 _mm256_storeu_si256((__m256i *)&l1_layer[i], y);
-//             }
-//         }
-
-//         void oneInput(int idx)
-//         {
-//             //update first hidden layer, assuming input bit set to one.
-//             __m256i x, y;
-//             for (int i=0;i<L1_COUNT;i+=16)
-//             {
-//                 x = _mm256_loadu_si256((__m256i *)&input_weights[idx][i]);
-//                 y = _mm256_loadu_si256((__m256i *)&l1_layer[i]);
-//                 y = _mm256_add_epi16(y, x);
-//                 _mm256_storeu_si256((__m256i *)&l1_layer[i], y);
-//             }
-//         }
-
-//         int forward()
-//         {
-//             //propagate from first hidden layer to output with AVX2.
-
-//             __m256i x, y, z;
-
-//             //L1 -> cReLU(L1)
-//             for (int i=0;i<L1_COUNT;i+=16)
-//             {
-//                 x = _mm256_loadu_si256((__m256i *)&l1_layer[i]);
-//                 x = _mm256_max_epi16(_ZERO, x);
-//                 x = _mm256_min_epi16(_CRELU1, x);
-//                 _mm256_storeu_si256((__m256i *)&l1_crelu[i], x);
-//             }
-
-//             //cReLU(L1) -> L2
-//             for (int i=0;i<L2_COUNT;i++)
-//             {
-//                 z = _mm256_setzero_si256();
-//                 for (int j=0;j<L1_COUNT;j+=16)
-//                 {
-//                     x = _mm256_loadu_si256((__m256i *)&l1_crelu[j]);
-//                     y = _mm256_loadu_si256((__m256i *)&l1_weights[i][j]);
-//                     z = _mm256_add_epi32(z, _mm256_madd_epi16(x, y));
-//                 }
-//                 l2_layer[i] = hsum_8x32(z) + l1_bias[i];
-//             }
-
-//             //L2 -> cReLU(L2)
-//             for (int i=0;i<L2_COUNT;i+=8)
-//             {
-//                 x = _mm256_loadu_si256((__m256i *)&l2_layer[i]);
-//                 x = _mm256_max_epi32(_ZERO, x);
-//                 x = _mm256_min_epi32(_CRELU2, x);
-//                 x = _mm256_srai_epi32(x, CRELU2_RSHIFT);
-//                 _mm256_storeu_si256((__m256i *)&l2_layer[i], x);
-//             }
-
-//             //cReLU(L2) -> Output
-//             z = _mm256_setzero_si256();
-//             for (int i=0;i<L2_COUNT;i+=8)
-//             {
-//                 x = _mm256_loadu_si256((__m256i *)&l2_layer[i]);
-//                 y = _mm256_loadu_si256((__m256i *)&l2_weights[0][i]);
-//                 z = _mm256_add_epi32(z, _mm256_mullo_epi32(x, y));
-//             }
-//             output_layer[0] = (hsum_8x32(z) + l2_bias[0]) / OUTPUT_FACTOR;
-
-//             return output_layer[0];
-//         }
-// };
 
 #endif // NNUE_H_INCLUDED
