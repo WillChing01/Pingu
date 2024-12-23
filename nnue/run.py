553fd6d86951da318d15489ad29f04a7c336d3ea
WillChing01 <williamching2001@gmail.com>
2024-12-23 00:43:36 +0000

    update run.py

--- nnue/run.py
+++ nnue/run.py
@@ -1,44 +1,54 @@
 import os
+import shutil
 import subprocess
 
 CONFIG = {
     "download": {
-        "dir": f"{os.getcwd()}\\dataset",
-        "cmd": (
-            ("cmd", "/c", "del", "/Q", "/S", "\\_raw"),
-            ("python", "download.py", "-d", "_raw"),
-        ),
+        "cwd": f"{os.getcwd()}\\dataset",
+        "dirs": ["_raw"],
+        "args": ["python", "download.py"],
+    },
+    "unpack": {
+        "cwd": f"{os.getcwd()}\\dataset",
+        "args": ["python", "unpack.py"],
     },
     "parse": {
-        "dir": f"{os.getcwd()}\\dataset",
-        "cmd": (
-            ("cmd", "/c", "del", "/Q", "/S", "\\training"),
-            ("cmd", "/c", "del", "/Q", "/S", "\\validation"),
-            ("make", "clean"),
-            ("make",),
-            ("parse.exe", "-N", "6"),
-            ("make", "clean"),
-        ),
+        "cwd": f"{os.getcwd()}\\dataset",
+        "dirs": ["training", "validation"],
+        "make": True,
+        "args": ["parse.exe", "-N", "6"],
     },
     "train": {
-        "dir": f"{os.getcwd()}\\model",
-        "cmd": (
-            ("cmd", "/c", "del", "/Q", "/S", "\\checkpoints"),
-            ("make", "clean"),
-            ("make",),
-            ("python", "train.py"),
-            ("make", "clean"),
-            ("python", "export.py"),
-        ),
+        "cwd": f"{os.getcwd()}\\model",
+        "dirs": ["checkpoint"],
+        "make": True,
+        "args": ["python", "train.py"],
     },
+    "export": {
+        "cwd": f"{os.getcwd()}\\model",
+        "args": ["python", "export.py"],
+    }
 }
 
 
 def main():
-    for process in ("download", "parse", "train"):
-        config = CONFIG[process]
-        for x in config["cmd"]:
-            subprocess.run(x, cwd=config["dir"])
+    for name, config in CONFIG.items():
+        print(name)
+
+        for dir in config.get("dirs", []):
+            path = f"{config['cwd']}\\{dir}"
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+            os.mkdir(path)
+
+        if config.get("make"):
+            for args in (["make", "clean"], ["make"]):
+                subprocess.run(args, cwd=config["cwd"])
+
+        subprocess.run(config["args"], cwd=config["cwd"])
+
+        if config.get("make"):
+            subprocess.run(["make", "clean"], cwd=config["cwd"])
 
 
 if __name__ == "__main__":
