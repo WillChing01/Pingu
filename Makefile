871cb3bdfe090f683267ee17f8d1449a0ddddba5
WillChing01 <williamching2001@gmail.com>
2025-07-17 23:55:17 +0100

    embed weights

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CXX := g++
+OBJCOPY := objcopy
 
 SRC := main.cpp
 OBJ := main.o
@@ -7,10 +8,15 @@ CPPFLAGS := -Iinclude
 CXXFLAGS := -Wall -std=gnu++20 -fno-exceptions -m64 -O3 -funroll-loops -mavx -mavx2
 LDFLAGS := -s -static -static-libstdc++ -static-libgcc
 
+NNUE_WEIGHTS := $(wildcard weights/nnue/*.bin)
+NNUE_OBJ := $(NNUE_WEIGHTS:.bin=.o)
+
 ifeq ($(OS),Windows_NT)
+	OBJCOPY_FLAGS := -I binary -O pei-x86-64 -B i386
 	EXE := Pingu.exe
-	RM := del /q
+	RM = $(foreach f,$1,if exist "$(f)" del /q "$(f)" & ) rem
 else
+	OBJCOPY_FLAGS := -I binary -O elf64-x86-64 -B i386
 	EXE := Pingu
 	RM := rm -f
 endif
@@ -19,11 +25,14 @@ endif
 
 all: $(EXE)
 
-$(EXE): $(OBJ)
-	$(CXX) $(OBJ) -o $(EXE) $(LDFLAGS)
+$(EXE): $(OBJ) $(NNUE_OBJ)
+	$(CXX) $(OBJ) $(NNUE_OBJ) -o $(EXE) $(LDFLAGS)
 
 $(OBJ): $(SRC)
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(SRC) -o $(OBJ)
 
+$(NNUE_OBJ): %.o: %.bin
+	$(OBJCOPY) $(OBJCOPY_FLAGS) $< $@
+
 clean:
-	$(RM) $(OBJ) $(EXE)
+	$(call RM,$(OBJ) $(NNUE_OBJ) $(EXE))
--- include/weights.h
+++ include/weights.h
@@ -1,16 +1,23 @@
 #ifndef WEIGHTS_H_INCLUDED
 #define WEIGHTS_H_INCLUDED
 
-extern const short perspective_w0_short_45056_32[];
-const short (*perspective_w0)[32] = reinterpret_cast<const short (*)[32]>(perspective_w0_short_45056_32);
+#include <array>
 
-extern const short perspective_b0_short_32[];
-const short* perspective_b0 = reinterpret_cast<const short*>(perspective_b0_short_32);
+extern const short _binary_weights_nnue_perspective_w0_short_45056_32_bin_start[];
+const std::array<std::array<short, 32>, 45056>& perspective_w0 =
+    *reinterpret_cast<const std::array<std::array<short, 32>, 45056>*>(
+        _binary_weights_nnue_perspective_w0_short_45056_32_bin_start);
 
-extern const char stacks_w0_char_4_64[];
-const char (*stacks_w0)[64] = reinterpret_cast<const char (*)[64]>(stacks_w0_char_4_64);
+extern const short _binary_weights_nnue_perspective_b0_short_32_bin_start[];
+const std::array<short, 32>& perspective_b0 =
+    *reinterpret_cast<const std::array<short, 32>*>(_binary_weights_nnue_perspective_b0_short_32_bin_start);
 
-extern const int stacks_b0_int_4[];
-const int* stacks_b0 = reinterpret_cast<const int*>(stacks_b0_int_4);
+extern const char _binary_weights_nnue_stacks_w0_char_4_64_bin_start[];
+const std::array<std::array<char, 64>, 4>& stacks_w0 =
+    *reinterpret_cast<const std::array<std::array<char, 64>, 4>*>(_binary_weights_nnue_stacks_w0_char_4_64_bin_start);
+
+extern const int _binary_weights_nnue_stacks_b0_int_4_bin_start[];
+const std::array<int, 4>& stacks_b0 =
+    *reinterpret_cast<const std::array<int, 4>*>(_binary_weights_nnue_stacks_b0_int_4_bin_start);
 
 #endif // WEIGHTS_H_INCLUDED
