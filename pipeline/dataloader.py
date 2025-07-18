088fa0ef6f7ad4f863efc5cef1f45b102b02c0cb
WillChing01 <williamching2001@gmail.com>
2025-07-18 17:00:56 +0100

    remove unused dataloader base class

--- pipeline/dataloader.py
+++
@@ -1,9 +0,0 @@
-class DataLoader:
-    def __init__(self, kind):
-        self.kind = kind
-
-    def __len__(self):
-        raise NotImplementedError
-
-    def iterator(self):
-        raise NotImplementedError
