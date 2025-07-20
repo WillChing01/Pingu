c498b7713eecbc02fe1dd73147c26b761aeb9ef5
WillChing01 <williamching2001@gmail.com>
2025-07-20 17:22:20 +0100

    add time test script

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include "thread.h"
+#include "time-network.h"
+
+int main(int argc, const char** argv) {
+    populateMagicTables();
+    populateRandomNums();
+    populateLmrTable();
+
+    Thread thread;
+    TimeNetwork network = TimeNetwork(&thread);
+
+    if (argc == 7) {
+        thread.b.setPositionFen(argv[1]);
+        std::cout << network.forward(1, 0, 1) << std::endl;
+    }
+
+    delete[] hashTable;
+
+    return 0;
+}
